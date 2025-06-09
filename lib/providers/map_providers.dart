449b2349dccfc8377844539b61698c5c1ede8e8e
Solomon <solomon@mycovergenius.com>
2025-06-09 18:45:14 +0100

    wip

--- lib/providers/map_providers.dart
+++ lib/providers/map_providers.dart
@@ -1,4 +1,4 @@
-import 'package:delivery_app/controllers/google_maps/user_location_controller.dart';
+import 'package:delivery_app/controllers/maps/user_location_controller.dart';
 import 'package:delivery_app/models/user_location_model.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
